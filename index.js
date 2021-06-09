3a4170d447a73ae9d6e9d031046ea358d01decc4
Giri Jeedigunta <giri.jeedigunta@gmail.com>
2021-06-09 10:28:51 +0530

    code cleanup

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-import { v4 as uuidv4 } from 'uuid';
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify(`Hello from Lambda and Github! ${uuidv4()}`),
+        body: JSON.stringify(`Hello from Lambda and Github!}`),
     };
     return response;
 };
