6bd7faeccd760ec8eebe963fd57266fc389d0566
Giri Jeedigunta <giri.jeedigunta@gmail.com>
2021-06-09 10:27:13 +0530

    added a new package for uuid

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+import { v4 as uuidv4 } from 'uuid';
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda and Github!'),
+        body: JSON.stringify(`Hello from Lambda and Github! ${uuidv4()}`),
     };
     return response;
 };
